86c2749882d892d1c40acc8eb319c4141fd49ce9
InFamousSwith <gitlin.boris191@yandex.ru>
2022-03-20 20:24:55 +0300

    kjnb

--- project/simpleapp/urls.py
+++ project/simpleapp/urls.py
@@ -4,7 +4,7 @@ from .views import Products, ProductDetailView, ProductCreateView, ProductUpdate
 urlpatterns = [
     path('', Products.as_view()),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # Ссылка на детали товара
-    path('create/', ProductCreateView.as_view(), name='product_create'),  # Ссылка на создание товара
-    path('create/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
-    path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
+    path('add/', ProductCreateView.as_view(), name='product_create'),  # Ссылка на создание товара
+    path('<int:pk>/edit', ProductUpdateView.as_view(), name='product_update'),
+    path('<int:pk>/delete', ProductDeleteView.as_view(), name='product_delete'),
 ]
